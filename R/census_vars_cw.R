bd86daa8a9939152b8d28635ec1c4a9630d0cc27
Hasan K. Tosun <hktosun@gmail.com>
2021-06-02 11:14:55 -0500

    Add add_census

--- R/census_vars_cw.R
+++ R/census_vars_cw.R
@@ -9,13 +9,11 @@
 # 		"kids", "P012003", "under5_male",
 # 		"kids", "P012027", "under5_female")
 #
-acs_vars <- tibble::tribble(
-		~variable_cat, ~variable_code, ~variable_label,
-		"kids", "B06001_002E", "population_under5",
-		"median income", "B06011_001E", "median_income",
-		"preschool enrollment", "B14001_003E", "preschool_enrollment",
-		"public preschool enrollment", "B14003_004E", "public_preschool_enrollment")
-
-use_data(census_vars, acs_vars, internal = TRUE, overwrite = TRUE)
-
-B19101_001E
+# acs_vars <- tibble::tribble(
+# 		~variable_cat, ~variable_code, ~variable_label,
+# 		"kids", "B06001_002E", "population_under5",
+# 		"median income", "B06011_001E", "median_income",
+# 		"preschool enrollment", "B14001_003E", "preschool_enrollment",
+# 		"public preschool enrollment", "B14003_004E", "public_preschool_enrollment")
+#
+# use_data(census_vars, acs_vars, internal = TRUE, overwrite = TRUE)
