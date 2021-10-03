e16ad0c5a54e782b4a27cb9ffa6d427fcbea3b8d
Hasan K. Tosun <hktosun@gmail.com>
2021-10-03 14:53:18 -0500

    add def'n

--- R/to_planar.R
+++ R/to_planar.R
@@ -1,5 +1,10 @@
-
-
+#' Convert long/lat to planar coordinates
+#'
+#' Convert long/;at coordinates to planar coordinates.
+#'
+#' @param df sf tibble
+#'
+#' @return An sf tibble
 #' @export
 
 to_planar <- function(df){
@@ -7,6 +12,13 @@ to_planar <- function(df){
 		sf::st_transform("+proj=utm +zone=15N +datum=NAD83 +units=m")
 }
 
+#' Convert planar to long/lat coordinates
+#'
+#' Convert planar coordinates to long/lat coordinates.
+#'
+#' @param df sf tibble
+#'
+#' @return An sf tibble
 #' @export
 #'
 to_longlat <- function(df){
