410e29089cfccb2522230d497b58c88696c6017c
Hasan K. Tosun <hktosun@gmail.com>
2021-10-03 14:53:06 -0500

    change def'n and add .data

--- R/read_vpk.R
+++ R/read_vpk.R
@@ -1,9 +1,12 @@
-#' Load VPK data
+#' Read VPK data
 #'
-#' Load VPK data using various sources
+#' Read VPK data using various sources
 #'
 #' @param source "enrollment" or "allocation"
+#' @param geography Geographic unit attached to the VPK data
+#' @param geo_year Vintage of the geography
 #'
+#' @return A tibble
 #' @export
 #'
 #'
@@ -11,16 +14,16 @@ read_vpk <- function(source = "enrollment", geography = "census-place", geo_year
 
 	if(source == "enrollment"){
 		vpk_locs <- read_public() %>%
-			dplyr::filter(grade == "PK", year >= 2017) %>%
+			dplyr::filter(.data$grade == "PK", .data$year >= 2017) %>%
 			dplyr::mutate(school_district_id = dplyr::case_when(
-				school_district_id == "2909-01" & year == 2021 ~ "0706-01",
-				TRUE ~ school_district_id
+				.data$school_district_id == "2909-01" & .data$year == 2021 ~ "0706-01",
+				TRUE ~ .data$school_district_id
 			))
 
 		df <- vpk_locs %>%
 			dplyr::left_join(get_geometry("school-location", 2020), by = c("school_district_id", "school_number")) %>%
 			sf::st_as_sf() %>%
-			dplyr::select(year, school_district_id, school_number, school_name, n = count)
+			dplyr::select(.data$year, .data$school_district_id, .data$school_number, .data$school_name, n = .data$count)
 
 
 		df <- df %>%
